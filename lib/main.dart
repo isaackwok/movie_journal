cf946140fe844bdf5219a17981d09d11f594d95a
Isaac Kwok <isaac.kwok@utonia.com>
2025-05-22 16:04:34 +0800

    feat: force mobile layout on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,7 +7,26 @@ import 'package:movie_journal/themes.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
-  runApp(const ProviderScope(child: MyApp()));
+  final runnableApp = _buildRunnableApp(
+    isWeb: kIsWeb,
+    webAppWidth: 400,
+    app: const ProviderScope(child: MyApp()),
+  );
+  runApp(runnableApp);
+}
+
+Widget _buildRunnableApp({
+  required bool isWeb,
+  required double webAppWidth,
+  required Widget app,
+}) {
+  if (!isWeb) {
+    return app;
+  }
+
+  return Center(
+    child: ClipRect(child: SizedBox(width: webAppWidth, child: app)),
+  );
 }
 
 class MyApp extends StatelessWidget {
