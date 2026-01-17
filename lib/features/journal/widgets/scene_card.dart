7af7fd4ee3f29e1d97e41507a311007c698c4159
Isaac <kch062522@gmail.com>
2026-01-17 10:22:19 +0800

    feat: adjus scene card appearance
    Signed-off-by: Isaac <kch062522@gmail.com>

--- lib/features/journal/widgets/scene_card.dart
+++ lib/features/journal/widgets/scene_card.dart
@@ -44,14 +44,22 @@ class _SceneCardState extends State<SceneCard> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.isEditable) {
+      return _buildEditableCard();
+    } else {
+      return _buildReadOnlyCard();
+    }
+  }
+
+  Widget _buildEditableCard() {
     return Column(
       spacing: 0,
       mainAxisSize: MainAxisSize.min,
       children: [
         ClipRRect(
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(12),
-            topRight: Radius.circular(12),
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
           ),
           child: Image.network(
             'https://image.tmdb.org/t/p/w500${widget.imagePath}',
@@ -73,7 +81,7 @@ class _SceneCardState extends State<SceneCard> {
           maxLines: 2,
           minLines: 1,
           decoration: InputDecoration(
-            hintText: widget.isEditable ? 'Add a caption...' : '<No caption>',
+            hintText: 'Add a caption...',
             hintStyle: TextStyle(
               color: Colors.white.withAlpha(153),
               fontSize: 13,
@@ -107,6 +115,123 @@ class _SceneCardState extends State<SceneCard> {
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildReadOnlyCard() {
+    final caption = widget.caption;
+    final hasCaption = caption != null && caption.isNotEmpty;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Stack(
+        children: [
+          Image.network(
+            'https://image.tmdb.org/t/p/w500${widget.imagePath}',
+            width: double.infinity,
+            height: 205,
+            fit: BoxFit.cover,
+          ),
+          if (hasCaption)
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: _buildCaptionOverlay(caption),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCaptionOverlay(String caption) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.transparent, Colors.black.withAlpha(200)],
+        ),
+      ),
+      padding: EdgeInsets.fromLTRB(12, 24, 12, 8),
+      child: _buildCaptionText(caption),
+    );
+  }
+
+  Widget _buildCaptionText(String caption) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 13,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'AvenirNext',
+      height: 1.4,
+    );
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final textSpan = TextSpan(text: caption, style: textStyle);
+        final textPainter = TextPainter(
+          text: textSpan,
+          maxLines: 2,
+          textDirection: TextDirection.ltr,
+        )..layout(maxWidth: constraints.maxWidth);
+
+        final isOverflowing = textPainter.didExceedMaxLines;
+
+        if (!isOverflowing) {
+          return Text(
+            caption,
+            style: textStyle,
+            maxLines: 2,
+          );
+        }
+
+        // Find truncation point - calculate how much text fits with "...more" suffix
+        const moreText = '...more';
+        var truncatedText = caption;
+        var truncatedPainter = TextPainter(
+          text: TextSpan(text: '$truncatedText$moreText', style: textStyle),
+          maxLines: 2,
+          textDirection: TextDirection.ltr,
+        )..layout(maxWidth: constraints.maxWidth);
+
+        while (truncatedPainter.didExceedMaxLines && truncatedText.isNotEmpty) {
+          truncatedText = truncatedText.substring(0, truncatedText.length - 1);
+          truncatedPainter = TextPainter(
+            text: TextSpan(text: '$truncatedText$moreText', style: textStyle),
+            maxLines: 2,
+            textDirection: TextDirection.ltr,
+          )..layout(maxWidth: constraints.maxWidth);
+        }
+
+        // Trim trailing spaces and punctuation for cleaner look
+        truncatedText = truncatedText.trimRight();
+
+        return GestureDetector(
+          onTap: () {
+            // TODO: Expand caption on tap
+          },
+          child: RichText(
+            maxLines: 2,
+            text: TextSpan(
+              style: textStyle,
+              children: [
+                TextSpan(text: truncatedText),
+                TextSpan(
+                  text: '...',
+                  style: textStyle,
+                ),
+                TextSpan(
+                  text: 'more',
+                  style: textStyle.copyWith(
+                    color: Colors.white.withAlpha(128),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
